4983c2fb3c258e867ec7c18f95ae95bdcc125246
starlying <starlying@gmail.com>
2019-01-08 07:52:48 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using SiteServer.Plugin;
+
+namespace Theme800.RQCode
+{
+    public class Main : PluginBase
+    {
+        public const string PluginId = "Theme800.RQCode";
+        public const string ElementName = "stl:qrcode";
+
+        private const string AttributeUrl = "url";
+        private const string AttributeSize = "size";
+
+        public static string Parse(IParseContext context)
+        {
+            var url = string.Empty;
+            var size = 0;
+
+            foreach (var name in context.StlAttributes.AllKeys)
+            {
+                var value = context.StlAttributes[name];
+
+                if (name.ToLower() == AttributeUrl)
+                {
+                    url = Context.ParseApi.ParseAttributeValue(value, context);
+                }
+                else if (name.ToLower() == AttributeSize)
+                {
+                    int.TryParse(value, out size);
+                }
+            }
+
+            url = string.IsNullOrEmpty(url) ? "location.href" : $"'{url}'";
+
+            var guid = Guid.NewGuid().ToString();
+            var libUrl = Context.PluginApi.GetPluginUrl(PluginId, "qrcode.min.js");
+
+            return size == 0
+                ? $@"
+<div class=""qrcode"" id=""{guid}""></div>
+<script type=""text/javascript"" src=""{libUrl}""></script>
+<script type=""text/javascript"">
+new QRCode(document.getElementById('{guid}'), {url});
+</script>
+"
+                : $@"
+<div class=""qrcode"" id=""{guid}""></div>
+<script type=""text/javascript"" src=""{libUrl}""></script>
+<script type=""text/javascript"">
+new QRCode(document.getElementById('{guid}'), {{
+    text: {url},
+	width: {size},
+	height: {size}
+}});
+</script>
+";
+        }
+
+        public override void Startup(IService service)
+        {
+            service.AddStlElementParser(ElementName, Parse);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# QRCode
-显示二维码
+# Theme800.QRCode 二维码插件
+
+二维码插件用于在模板中通过标签将 URL 地址生成二维码图片并展示在页面中。
+
+## 基本用法
+
+二维码插件使用 stl:qrCode 标签生成二维码，二维码的地址为当前页面地址：
+
+```
+<stl:qrCode></stl:qrCode>
+```
+
+## 指定二维码地址
+
+可以使用 url 属性指定二维码地址：
+
+```
+<stl:qrCode url="https://www.siteserver.cn"></stl:qrCode>
+```
+
+## 指定二维码图片尺寸
+
+可以使用 size 属性指定二维码图片尺寸：
+
+```
+<stl:qrCode size="128"></stl:qrCode>
+```
+
+## 源代码
+
+您可以在 [https://github.com/theme800/Theme800.RQCode](https://github.com/theme800/Theme800.RQCode) 中查看并获取二维码插件最新源代码。
